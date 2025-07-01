3b5e1b10d4b35fb05c8f23bd0410ee665c39d844
ark948 <armankarimi948@gmail.com>
2025-07-01 13:06:28 +0330

    Added new signup form

--- backend/accounts/forms.py
+++ backend/accounts/forms.py
@@ -3,3 +3,18 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 from .models import CustomUser
 
+class CustomUserSignUpForm(UserCreationForm):
+
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(attrs={
+            'type': 'email',
+            'id': 'email'
+        })
+    )
+
+    class Meta:
+        model = CustomUser
+        fields = (
+            "email",
+        )
