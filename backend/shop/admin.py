f767b6ac4a5e2e363c443272b5ac7c7d4a43bb8d
ark948 <armankarimi948@gmail.com>
2025-07-07 22:51:10 +0330

    Preparations before making migrations - ready

--- backend/shop/admin.py
+++ backend/shop/admin.py
@@ -1,3 +1,52 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import (
+    Country,
+    Language,
+    Translator,
+    Illustrator,
+    Author,
+    Genre,
+    Tag,
+    Keyword,
+    Publication,
+    OriginalLanguage,
+    Format,
+    Size,
+    Series,
+    AgeRecommendation,
+    Book,
+    Award,
+    Review,
+    Discount,
+    Comment,
+    Order,
+    Payment,
+    Invoice,
+)
+
+
+admin.site.register(Country)
+admin.site.register(Language)
+admin.site.register(Illustrator)
+admin.site.register(Translator)
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(Tag)
+admin.site.register(Keyword)
+admin.site.register(Publication)
+admin.site.register(OriginalLanguage)
+admin.site.register(Format)
+admin.site.register(Size)
+admin.site.register(Series)
+admin.site.register(AgeRecommendation)
+admin.site.register(Book)
+admin.site.register(Award)
+admin.site.register(Review)
+admin.site.register(Discount)
+admin.site.register(Comment)
+admin.site.register(Order)
+admin.site.register(Payment)
+admin.site.register(Invoice)
