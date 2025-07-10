e07fdb42c4d0d91d5ee84afa4a733c7edff39425
ark948 <armankarimi948@gmail.com>
2025-07-10 12:29:56 +0330

    Fixed login bug

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -6,7 +6,7 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
-    path('login/', views.login, name='login'),
+    path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('protected-page/', views.protected_view, name='prtd-page'),
 ]
