b1a2e58286f5ddece62c8fb7815334b6652db7b8
ark948 <armankarimi948@gmail.com>
2025-07-10 19:21:44 +0330

    Added some fixtures

--- backend/shop/tests/conftest.py
+++ backend/shop/tests/conftest.py
@@ -1,9 +1,18 @@
 import pytest
 
-from shop.models import *
+from shop.models import (
+    Country, Language
+)
 
 @pytest.fixture
 def country():
     us = Country.objects.create(name="United States")
     us.save()
     return us
+
+
+@pytest.fixture
+def language():
+    en = Language.objects.create(name='English')
+    en.save()
+    return en
--- backend/shop/tests/test_models.py
+++ backend/shop/tests/test_models.py
@@ -3,6 +3,19 @@ import pytest
 from shop.models import *
 
 
+# OK
+@pytest.mark.skip
 @pytest.mark.django_db
 def test_country_fixture(country):
     print(country)
+    print(country.pk)
+    print(country.name)
+
+
+# OK
+@pytest.mark.skip
+@pytest.mark.django_db
+def test_langauge_fixtre(language):
+    print(language)
+    print(language.pk)
+    print(language.name)
