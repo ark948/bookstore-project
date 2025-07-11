ccedefd996b880e7260a70e65e9a4366c4ac1be2
ark948 <armankarimi948@gmail.com>
2025-07-11 22:08:07 +0330

    Added urls placeholder

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('accounts.urls')),
+    path('shop/', include('shop.urls')),
     path('', include('home.urls')),
 ]
 
