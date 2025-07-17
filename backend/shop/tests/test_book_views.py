390b1421d618b802db1685eb841ac586614f7604
ark948 <armankarimi948@gmail.com>
2025-07-17 22:14:11 +0330

    Updated tests

--- backend/shop/tests/test_book_views.py
+++ backend/shop/tests/test_book_views.py
@@ -1,12 +1,23 @@
+import urllib, requests
 import pytest
 from django.test import TestCase
 from django.urls import reverse
 from pytest_django.asserts import assertTemplateUsed
 
-from accounts.tests.conftest import user, custom_user
+from accounts.tests.conftest import user, custom_user, custom_employee
 
-@pytest.mark.skip
+
 @pytest.mark.django_db
-def test_books_list_inaccessible(client, user, book):
-    response = client.get(reverse("shop:books-list"))
+def test_books_list_inaccessible(client, custom_user):
+    client.force_login(custom_user)
+    response = client.get(reverse('shop:books-list'))
+
     assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_books_list(client, custom_employee):
+    client.login(username='user1@email.com', password='test123*A')
+    response = client.get(reverse("shop:books-list"))
+
+    assert response.status_code == 200
