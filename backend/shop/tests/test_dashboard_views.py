e7452d407522445d59cdc503d223cf834ed8c740
ark948 <armankarimi948@gmail.com>
2025-07-18 20:50:09 +0330

    Some tests were incorrect

--- backend/shop/tests/test_dashboard_views.py
+++ backend/shop/tests/test_dashboard_views.py
@@ -6,24 +6,52 @@ from accounts.tests.conftest import user, custom_user, custom_employee, custom_m
 
 
 @pytest.mark.django_db
-def test_admin_dashboard_inaccessible(custom_employee, client):
-    response = client.get(reverse('shop:employee'))
+def test_admin_dashboard_inaccessible_by_employee(custom_employee, client):
+    client.force_login(custom_employee)
+    response = client.get(reverse('shop:admin'))
     assert response.status_code == 403
 
 @pytest.mark.django_db
-def test_admin_dashboard(custom_employee, client):
-    client.force_login(custom_employee)
-    response = client.get(reverse('shop:employee'))
+def test_admin_dashboard_inaccessible_by_manager(custom_manager, client):
+    client.force_login(custom_manager)
+    response = client.get(reverse("shop:admin"))
+    assert response.status_code == 403
+
+@pytest.mark.django_db
+def test_admin_dashboard_accessible_only_to_admin(custom_admin, client):
+    client.force_login(custom_admin)
+    response = client.get(reverse('shop:admin'))
     assert response.status_code == 200
 
 
 @pytest.mark.django_db
-def test_manager_dashboard_inaccessible(custom_employee, client):
+def test_manager_dashboard_inaccessible_by_employee(custom_employee, client):
+    client.force_login(custom_employee)
     response = client.get(reverse('shop:manager'))
     assert response.status_code == 403
 
 @pytest.mark.django_db
-def test_manager_dashboard(custom_manager, client):
-    client.force_login(custom_manager)
+def test_manager_dashboard_inaccessible_by_admin(custom_admin, client):
+    client.force_login(custom_admin)
     response = client.get(reverse('shop:manager'))
+    assert response.status_code == 403
+
+@pytest.mark.django_db
+def test_manager_dashboard_accessible_only_to_manager(custom_manager, client):
+    client.force_login(custom_manager)
+    response = client.get(reverse("shop:manager"))
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_employee_dashboard_inaccessible_by_user(custom_user, client):
+    client.force_login(custom_user)
+    response = client.get(reverse('shop:employee'))
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_employee_dashboard_accessible_only_to_employee(custom_employee, client):
+    client.force_login(custom_employee)
+    response = client.get(reverse('shop:employee'))
     assert response.status_code == 200
