9609121c69f92c07a76715d97b41dae8a59e789e
ark948 <armankarimi948@gmail.com>
2025-08-06 23:13:51 +0330

    Updating tests structures

--- backend/shop/factories.py
+++ backend/shop/factories.py
@@ -2,5 +2,8 @@ import factory
 from datetime import datetime
 
 from .models import Author, Book, Country, Language, Publication
+from accounts.factories import CustomEmployeeFactory
 
 # Add factories here
+
+    
--- backend/shop/tests/conftest.py
+++ backend/shop/tests/conftest.py
@@ -52,3 +52,31 @@ def book(language, author, publication):
     book_obj.authors.set([author])
     book_obj.save()
     return book_obj
+
+
+@pytest.fixture
+def book_obj():
+    country = Country.objects.create(name="United States")
+    language = Language.objects.create(name="English")
+    author = Author.objects.create(en_name="Some Dude", nationality=country)
+    publication = Publication.objects.create(title="Good Books Publications", country=country)
+    genre = Genre.objects.create(title="Drama")
+    book = Book.objects.create(
+        title = "One Good Book",
+        publisher = publication,
+        language = language,
+        original_language = language,
+        page_count = 200,
+    )
+    book.authors.set([author])
+    book.genres.set([genre])
+    obj = {
+        'country': country,
+        'language': language,
+        'author': author,
+        'publication': publication,
+        'genre': genre,
+        'book': book
+    }
+
+    return obj
