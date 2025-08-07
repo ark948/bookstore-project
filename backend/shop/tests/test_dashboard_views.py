0b5a824ff4fb8abdb1e4fe82c932cf72524e0fce
ark948 <armankarimi948@gmail.com>
2025-08-07 19:48:24 +0330

    Tests running OK (Using pytest-lazy-fixtures)

--- backend/shop/tests/test_dashboard_views.py
+++ backend/shop/tests/test_dashboard_views.py
@@ -1,8 +1,9 @@
 import pytest
 from django.urls import reverse
 from pytest_django.asserts import assertTemplateUsed, assertRaisesMessage
+from pytest_lazy_fixtures import lf
 
-from accounts.tests.conftest import users
+from accounts.tests.conftest import users, custom_employee
 from accounts.models import CustomUser
 
 
@@ -23,21 +24,27 @@ def test_employee_dashboard(client, role, expected_status):
     assert response.status_code == expected_status
 
 
+@pytest.fixture
+def user():
+    return CustomUser.objects.create_user(email='user@example.com', password="pass", role="user")
+
+@pytest.fixture
+def manager_user():
+    return CustomUser.objects.create_user(email='manager@example.com', password="pass", role="manager")
+
+@pytest.fixture
+def employee_user():
+    return CustomUser.objects.create_user(email='employee@email.com', password="pass", role="employee")
+
 
 @pytest.mark.django_db
-@pytest.mark.parametrize("role, expected_status", [
-    ("user", 403),
-    ("manager", 200)
+@pytest.mark.parametrize("user_fixture, expected_status", [
+    (lf('user'), 403),
+    (lf('employee_user'), 200),
 ])
-def test_manager_dashboard(client, role, expected_status):
-    user = CustomUser.objects.create(
-        email='manager@example.com',
-        password='test123*A',
-        role=role
-    )
-
-    client.force_login(user)
-    response = client.get(reverse('shop:manager'))
+def test_manager_dashboard(client, user_fixture, expected_status):
+    client.force_login(user_fixture)
+    response = client.get(reverse('shop:employee'))
     assert response.status_code == expected_status
 
 
