a273b0c90a5cce72e04c8478efbe2f1c8c9fa2cd
ark948 <armankarimi948@gmail.com>
2025-08-12 22:29:19 +0330

    Customers browse page

--- backend/shop/customers/urls.py
+++ backend/shop/customers/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("browse/", views.browse, name='customers_browse'),
     path("", views.index, name='customers_index'),
 ]
--- backend/shop/customers/views.py
+++ backend/shop/customers/views.py
@@ -4,3 +4,7 @@ from django.http import HttpRequest, HttpResponse
 
 def index(request: HttpRequest) -> HttpResponse:
     return render(request, "shop/customers/index.html", {})
+
+
+def browse(request: HttpRequest) -> HttpResponse:
+    return render(request, "shop/customers/browse.html", {})
