83e7edbeb0a9313c951392612303c031e7779952
ark948 <armankarimi948@gmail.com>
2025-08-12 22:32:00 +0330

    Added test

---
+++ backend/shop/tests/test_customers_views.py
@@ -0,0 +1,21 @@
+import pytest
+import logging
+
+from django.urls import reverse
+from pytest_django.asserts import assertTemplateUsed
+
+
+@pytest.mark.django_db
+def test_customers_index_page(client):
+    response = client.get(reverse("shop:customers_index"))
+
+    assert response.status_code == 200
+    assertTemplateUsed(response, "shop/customers/index.html")
+
+
+@pytest.mark.django_db
+def test_customers_browse_page(client):
+    response = client.get(reverse("shop:customers_browse"))
+
+    assert response.status_code == 200
+    assertTemplateUsed(response, "shop/customers/browse.html")
