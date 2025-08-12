799249e0edd3661c843a625d8668618d84638738
ark948 <armankarimi948@gmail.com>
2025-08-12 22:53:06 +0330

    loaded books in customers browse page

--- backend/shop/customers/views.py
+++ backend/shop/customers/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
 
+from shop.models import Book
+
 
 def index(request: HttpRequest) -> HttpResponse:
     return render(request, "shop/customers/index.html", {})
 
 
 def browse(request: HttpRequest) -> HttpResponse:
-    return render(request, "shop/customers/browse.html", {})
+    books = Book.objects.all()
+    return render(request, "shop/customers/browse.html", {"items": books})
