85d577b6f6e446ad3d6e7d9143c56a1877d30ff9
ark948 <armankarimi948@gmail.com>
2025-08-19 22:48:38 +0330

    adding items to cart view (needs to be further configured)

--- backend/shop/customers/views.py
+++ backend/shop/customers/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
 from django.http import HttpRequest, HttpResponse
 from django.views.decorators.http import require_POST
 
 from shop.models import Book
 from .cart import Cart
+from .forms import ItemAddForm
 
 
 def index(request: HttpRequest) -> HttpResponse:
@@ -14,4 +16,12 @@ def browse(request: HttpRequest) -> HttpResponse:
     books = Book.objects.all()
     return render(request, "shop/customers/browse.html", {"items": books})
 
-
+@require_POST
+def cart_add(request: HttpRequest, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Book, id=product_id)
+    form = ItemAddForm(request.POST)
+    if form.is_valid():
+        cd = form.cleaned_data
+        cart.add(product=product, quantity=cd['quantity'], override_quantity=cd['override'])
+    return redirect(reverse("shop:cart-details"))
