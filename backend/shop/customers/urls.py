531d1bc3de3ba5f898207eb2965abbc4e62f7be5
ark948 <armankarimi948@gmail.com>
2025-08-20 18:44:53 +0330

    Cart detail template

--- backend/shop/customers/urls.py
+++ backend/shop/customers/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("cart-detail", views.cart_detail, name='cart-detail'),
+    path("cart-add/<int:product_id>/", views.cart_add, name='cart-add'),
+    path("remove/<int:product_id>/", views.cart_remove, name='cart-remove'),
     path("browse/", views.browse, name='customers_browse'),
     path("", views.index, name='customers_index'),
 ]
