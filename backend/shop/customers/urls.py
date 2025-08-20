1dec8fe999491ab4cda9efb25f66b4641a42decb
ark948 <armankarimi948@gmail.com>
2025-08-20 21:38:10 +0330

    Fixed problem with item_details endpoint in customers

--- backend/shop/customers/urls.py
+++ backend/shop/customers/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("cart-detail", views.cart_detail, name='cart_detail'),
     path("cart-add/<int:product_id>/", views.cart_add, name='cart_add'),
     path("remove/<int:product_id>/", views.cart_remove, name='cart_remove'),
-    path("item-detail", views.item_detail, name='item_detail'),
+    path("item-detail/<int:id>/", views.item_detail, name='item_detail'),
     path("browse/", views.browse, name='customers_browse'),
     path("", views.index, name='customers_index'),
 ]
