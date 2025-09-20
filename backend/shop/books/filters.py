540e4f06e95886f0b2266534024b807061d6f280
ark948 <armankarimi948@gmail.com>
2025-09-20 22:31:57 +0330

    Added another filter field

--- backend/shop/books/filters.py
+++ backend/shop/books/filters.py
@@ -1,16 +1,24 @@
 import django_filters
 from shop.models import (
-    Book, Genre
+    Book, Genre, Language
 )
 
 class BookFilter(django_filters.FilterSet):
     genres = django_filters.ModelChoiceFilter(
         queryset=Genre.objects.all(),
-        empty_label="Any"
+        empty_label="Any",
+        label="ژانر"
+    )
+
+    language = django_filters.ModelChoiceFilter(
+        queryset=Language.objects.all(),
+        empty_label="همه",
+        label="زبان"
     )
 
     class Meta:
         model = Book
         fields = (
             "genres",
+            "language",
         )
