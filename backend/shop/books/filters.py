1db783db40ecf8adaf77c8256296bc74d87e2f5b
ark948 <armankarimi948@gmail.com>
2025-09-23 22:56:15 +0330

    New filter field (authors)

--- backend/shop/books/filters.py
+++ backend/shop/books/filters.py
@@ -1,10 +1,15 @@
 import django_filters
 import django_filters.widgets
 from shop.models import (
-    Book, Genre, Language, Publication, Tag
+    Book, Genre, Language, Publication, Tag, Author
 )
 
 class BookFilter(django_filters.FilterSet):
+    authors = django_filters.ModelMultipleChoiceFilter(
+        queryset=Author.objects.all(),
+        label="نویسنده",
+    )
+
     publisher = django_filters.ModelChoiceFilter(
         queryset=Publication.objects.all(),
         empty_label="Any",
@@ -16,6 +21,18 @@ class BookFilter(django_filters.FilterSet):
         empty_label="همه",
         label="زبان"
     )
+
+    original_language = django_filters.ModelChoiceFilter(
+        queryset=Language.objects.all(),
+        empty_label="همه",
+        label="زبان اصلی"
+    )
+
+    edition = django_filters.NumberFilter(
+        lookup_expr='exact'
+    )
+
+    page_count = django_filters.RangeFilter()
 
     genres = django_filters.ModelMultipleChoiceFilter(
         queryset=Genre.objects.all(),
@@ -30,8 +47,12 @@ class BookFilter(django_filters.FilterSet):
     class Meta:
         model = Book
         fields = (
+            "authors",
             "publisher",
             "language",
+            "original_language",
+            "edition",
+            "page_count",
             "genres",
             "tags",
         )
