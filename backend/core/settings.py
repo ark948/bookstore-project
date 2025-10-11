b5d9b8941b5a82b91fc9a39d198b29aa49f7bbcc
ark948 <armankarimi948@gmail.com>
2025-10-11 21:24:39 +0330

    Added environment variables

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -12,16 +12,19 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import environ
+
+env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z953o$z+v599j(zet6b05$w@(t0a&k2t^6cudq74cq^z%$w)ti'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
