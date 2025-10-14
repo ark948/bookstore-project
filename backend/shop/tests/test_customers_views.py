4cef411000e5b34635dd64369eda6321890f84d7
ark948 <armankarimi948@gmail.com>
2025-10-14 17:28:26 +0330

    +2 tests for customer add comment, add favorite

--- backend/shop/tests/test_customers_views.py
+++ backend/shop/tests/test_customers_views.py
@@ -4,6 +4,13 @@ import logging
 from django.urls import reverse
 from pytest_django.asserts import assertTemplateUsed
 
+from shop.models import (
+    Book,
+    Comment,
+    Favorite
+)
+from accounts.tests.conftest import user
+
 
 @pytest.mark.django_db
 def test_customers_index_page(client):
@@ -21,3 +28,45 @@ def test_customers_browse_page(client):
     assertTemplateUsed(response, "shop/customers/browse.html")
     assert "items" in response.context
     assert list(response.context['items']) == []
+
+
+# customer's comment test
+@pytest.mark.django_db
+def test_customers_add_comment(client, book_obj, user):
+    client.force_login(user)
+    total = Comment.objects.all().count()
+    assert total == 0
+    comment = {
+        'body': 'a nice comment.',
+        'book_id': book_obj['book'].pk
+    }
+
+    url = reverse('shop:add_comment')
+    response = client.post(url, data=comment)
+    assert response.status_code == 200
+
+    total = Comment.objects.all().count()
+    assert total == 1
+
+    comment = Comment.objects.get(pk=1)
+    assert comment.body == 'a nice comment.'
+
+
+@pytest.mark.django_db
+def test_customers_add_to_favorite(client, book_obj, user):
+    client.force_login(user)
+    total = Favorite.objects.all().count()
+    assert total == 0
+    headers = {'Hx-Request': 'true'}
+    data = {
+        'book_id': book_obj['book'].pk
+    }
+    url = reverse('shop:add_favorite')
+    response = client.get(url, data=data, headers=headers)
+    assert response.status_code == 200
+    total = Favorite.objects.all().count()
+    assert total == 1
+
+    favorite = Favorite.objects.get(pk=1)
+    assert favorite.user_id == user
+    assert favorite.book_id == book_obj['book']
