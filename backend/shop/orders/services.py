a72a75a1224a30a72db052127a4afe8c66139eaa
ark948 <armankarimi948@gmail.com>
2025-10-28 22:15:02 +0330

    Services for orders

---
+++ backend/shop/orders/services.py
@@ -0,0 +1,16 @@
+from typing import Dict, List, Optional, Required, Any
+from django.db.models import QuerySet
+from django.shortcuts import get_object_or_404
+
+from shop.models import Order
+
+
+def update_order_status(value: Any, order_id: Optional[int] = None, order_number: Optional[str] = None):
+    if not (order_id or order_number):
+        raise ValueError("Either order_id or order_number is required.")
+    if order_id:
+        order_obj = get_object_or_404(Order, pk=order_id)
+    elif order_number:
+        order_obj = get_object_or_404(Order, order_number=order_number)
+    order_obj.status = Order.ORDER_STATUSES[value]
+    
