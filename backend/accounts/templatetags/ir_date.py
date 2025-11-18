2a719cc10a08064bc4661ce376774eb3df500262
ark948 <armankarimi948@gmail.com>
2025-11-18 23:05:32 +0330

    updated to_persian filter (persian months)

--- backend/accounts/templatetags/ir_date.py
+++ backend/accounts/templatetags/ir_date.py
@@ -7,6 +7,39 @@ register = template.Library()
 @register.filter(expects_localtime=True)
 def to_persian(dt):
     """Converts datetime object to represent persian date and time."""
-    return persian.from_gregorian(
+    date = persian.from_gregorian(
         int(dt.strftime("%Y")), int(dt.strftime("%m")), int(dt.strftime("%d"))
     )
+    year = date[0]
+    month = None
+    day = date[2]
+    # These are persian months
+    match date[1]:
+        case 1:
+            month = "فروردین"
+        case 2:
+            month = "اردیبهشت"
+        case 3:
+            month = "خرداد"
+        case 4:
+            month = "تیر"
+        case 5:
+            month = "مرداد"
+        case 6:
+            month = "شهریور"
+        case 7:
+            month = "مهر"
+        case 8: 
+            month = "آبان"
+        case 9:
+            month = "آذر"
+        case 10:
+            month = "دی"
+        case 11:
+            month = "بهمن"
+        case 12:
+            month = "اسفند"
+        case _:
+            month = "ERR"
+
+    return f"{day} {month} {year}"
