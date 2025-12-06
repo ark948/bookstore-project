6929900a06ac13a81c49bed9033534f67a98fb94
ark948 <armankarimi948@gmail.com>
2025-12-06 23:04:11 +0330

    Refactoring... (updated home app)

--- backend/home/urls.py
+++ backend/home/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 
-from . import views
+from home import views
 
 app_name = 'home'
-
 urlpatterns = [
-    path('secret/', views.secret_view_test, name='secret-1'),
-    path('secret-2/', views.MySecretView.as_view(), name='secret-2'),
     path('about/', views.about, name='about'),
     path('contact-us/', views.contact_us, name='contact'),
     path('', views.index, name='index'),
+
+    # routes for development and testing
+    path('secret/', views.secret_view_test, name='secret_1'),
+    path('secret-2/', views.MySecretView.as_view(), name='secret_2'),
 ]
--- backend/home/views.py
+++ backend/home/views.py
@@ -1,41 +1,41 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test, permission_required
-from django.http import HttpResponseForbidden, HttpResponse, HttpRequest
+from django.http import HttpResponse, HttpRequest
 from django.views import View
 from django.utils.decorators import method_decorator
-
-from accounts.models import CustomUser
-
-# Create your views here.
 
 def index(request):
     return render(request, 'home/index.html')
 
+
 def about(request):
     return render(request, "home/about.html")
+
 
 def contact_us(request):
     return render(request, "home/contact.html")
 
+
 def has_custom_permission(user):
-    # Replace with your custom logic
     return user.is_authenticated and user.is_staff
 
+
 @user_passes_test(has_custom_permission, login_url='accounts//login/')
-def my_view(request):
+def my_view(request: HttpRequest) -> HttpResponse:
     return HttpResponse("You have access to this view!")
 
 
 @user_passes_test(lambda user: user.is_authenticated, login_url='accounts:login')
-def secret_view_test(request):
+def secret_view_test(request: HttpRequest) -> HttpResponse:
     return HttpResponse("Secret view.")
 
 
 @permission_required('home.employee_rights')
-def employee_view(request: HttpRequest):
+def employee_view(request: HttpRequest) -> HttpResponse:
     return HttpResponse(f"{request.user.username}, You have access to this view!")
 
-def origin_view(request):
+
+def origin_view(request: HttpRequest) -> HttpResponse:
     if request.user and request.user.has_perm('myapp.employee_rights'):
         return HttpResponse(f"{request.user.email}, You have access to this view!")
     return HttpResponse("You do have access to this view!")
@@ -48,5 +48,5 @@ class CustomPermissionMixin:
     
 
 class MySecretView(CustomPermissionMixin, View):
-    def get(self, request):
+    def get(self, request: HttpRequest):
         return HttpResponse("Secret class-based view.")
