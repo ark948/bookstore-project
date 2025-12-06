1e34b612d2a764a913de716b8495ed686fbc91d6
ark948 <armankarimi948@gmail.com>
2025-12-06 22:44:48 +0330

    Refactoring... (accounts urls)

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -5,27 +5,27 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('protected-page/', views.protected_view, name='prtd-page'),
-
     # CRUD actions
-    path("favorite-remove/<int:book_id>/", views.remove_item_from_favorites, name='remove_favorite'),
-    path("edit-comment/<int:comment_id>/", views.edit_comment, name='edit_comment'),
-    path("delete-comment/<int:comment_id>/", views.delete_user_comment, name='delete_comment'),
+    path("favorite-remove/<int:book_id>/", views.remove_item_from_favorites, name='acc_remove_favorite'),
+    path("edit-comment/<int:comment_id>/", views.edit_comment, name='acc_edit_comment'),
+    path("delete-comment/<int:comment_id>/", views.delete_user_comment, name='acc_delete_comment'),
 
     # loaders
-    path("load-orders/<str:status>", views.load_orders_with_status, name='load_orders_with_status'),
-    path("load-comment-form/<int:comment_id>/", views.load_comment_form_partial, name='load_comment_form'),
-    path("favorites-list/", views.favorites_list, name='fav_list'),
-    path("orders-list/", views.orders_list, name='orders_list'),
-    path("comments-list/", views.comments_list, name='comments_list'),
+    path("load-orders/<str:status>", views.load_orders_with_status, name='acc_load_orders_with_status'),
+    path("load-comment-form/<int:comment_id>/", views.load_comment_form_partial, name='acc_load_comment_form'),
+    path("favorites-list/", views.favorites_list, name='acc_fav_list'),
+    path("orders-list/", views.orders_list, name='acc_orders_list'),
+    path("comments-list/", views.comments_list, name='acc_comments_list'),
+    path('ajax/load-cities/', views.load_cities, name='acc_load_cities'),
 
     # Profile routes
-    path("profile/", views.profile, name='profile'),
-    path("address/<int:profile_id>/", views.add_address, name='add_address'),
-    path('ajax/load-cities/', views.load_cities, name='load_cities'),
+    path("profile/", views.profile, name='acc_profile'),
+    path("address/<int:profile_id>/", views.add_address, name='acc_add_address'),
 
     # Authentication routes
     path("signup/", views.signup, name='signup'),
     path("login/", views.login_view, name='login'),
     path("logout/", views.logout_view, name='logout'),
+
+    path('protected-page/', views.protected_view, name='acc_prtd_page'),
 ]
