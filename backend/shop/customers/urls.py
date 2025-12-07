4766fb0993b262456ddeced962d24ddf2f487a5c
ark948 <armankarimi948@gmail.com>
2025-12-07 22:43:46 +0330

    Moved shop/customers comments to its own folder

--- backend/shop/customers/urls.py
+++ backend/shop/customers/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 
 from shop.customers.cart import urls as cart_urls
 from shop.customers.books import urls as books_urls
+from shop.customers.comments import urls as comments_urls
 from . import views
 
 urlpatterns = [
@@ -10,13 +11,10 @@ urlpatterns = [
     path("add-to-favorite/<int:book_id>/", views.add_book_favorite, name='add_favorite'),
     path('is-book-favorite/<int:book_id>/', views.is_book_favorite, name='is_book_favorite'),
 
-    # comments
-    path("add-comment/<int:book_id>/", views.add_comment, name='add_comment'),
-    path('load-book-comments/<int:book_id>/', views.load_book_comments, name='load_book_comments'),
-
     # routes for employees
     path("list/", views.customers_list, name='customers_list'),
     path('books/', include(books_urls)),
+    path('comments/', include(comments_urls)),
     path("cart/", include(cart_urls)),
-    path("", views.index, name='customers_index'),
+    path('', views.index, name='customers_index'),
 ]
--- backend/shop/customers/views.py
+++ backend/shop/customers/views.py
@@ -1,52 +1,15 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpRequest, HttpResponse, JsonResponse, HttpResponseForbidden
-from django.views.decorators.http import require_POST
-from django.contrib import messages
+from django.http import HttpRequest, HttpResponse
 from django.db.models import QuerySet
-from django.db.utils import IntegrityError
-from django.template.loader import render_to_string
 
 from accounts.decorators import role_required
-from shop.models import Book, Comment, Favorite
+from shop.models import Book, Favorite
 from accounts.models import CustomUser
-from shop.customers.forms import AddCommentForm
 
 
 def index(request: HttpRequest) -> HttpResponse:
     return render(request, "shop/customers/index.html", {})
 
-
-@require_POST
-@role_required('user')
-def add_comment(request: HttpRequest, book_id: int) -> HttpResponse:
-    book = get_object_or_404(Book, id=book_id)
-    form = AddCommentForm(request.POST)
-    if form.is_valid():
-        # if all data is available and no aciton is needed before saving object, use create()
-        try:
-            Comment.objects.create(
-                title = form.cleaned_data['title'],
-                body = form.cleaned_data['body'],
-                book = book,
-                user = request.user,
-                anonymous = form.cleaned_data['anonymous']
-            )
-        except IntegrityError as unique_error:
-            html = render_to_string('shop/customers/errors/forbidden.html', {
-                'message': "شما قبلا برای این کتاب نظر ثبت کرده اید. در صورت نیاز لطفا آن را ویرایش کنید."
-            })
-            return HttpResponseForbidden(html)
-        # return a single comment item
-        # response = render(request, "shop/customers/partials/comment_item.html", {'comment': comment})
-        # response['HX-Trigger'] = "comment_successfully_submitted"
-        # in template, use hx-on:comment_successfully_submitted and hx-swap='beforeend' to listen to this event
-        # and add the returned comment to the end of the list
-        # return response
-        return HttpResponse("نظر شما دریافت شد و پس از تایید نمایش داده خواهد شد.")
-    else:
-        # this needs to be upated with comment_form template
-        messages.error(request, "خطایی در درج نظر رخ داد. لطفا دوباره تلاش کنید.")
-        return HttpResponse(form.errors)
 
     
 @role_required('user')
@@ -78,16 +41,6 @@ def is_book_favorite(request: HttpRequest, book_id: int) -> HttpResponse:
         if favorite:
             return render(request, "shop/customers/partials/remove_from_favorite_partial.html", {'book_id': book_id})
         return render(request, "shop/customers/partials/add_to_favorite_partial.html", {'book_id': book_id})
-
-
-@role_required('user')
-def load_book_comments(request: HttpRequest, book_id: int) -> HttpResponse:
-    if request.htmx:
-        try:
-            comments = Comment.objects.filter(book=book_id, status="Approved")
-        except Exception as error:
-            return JsonResponse("متاسفانه خطایی رخ داده است. لطفا صفحه را مجددا رفرش کنید.")
-        return render(request, "shop/customers/partials/book_item_comments.html", {'items': comments})
 
 
 @role_required('employee')
