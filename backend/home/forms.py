3ffb2ff289fbe9f38977cc3b81deb3fcca0b085d
ark948 <armankarimi948@gmail.com>
2025-12-14 14:26:41 +0330

    Updated contact us form to use textarea

--- backend/home/forms.py
+++ backend/home/forms.py
@@ -1,8 +1,12 @@
 from django.forms.models import ModelForm
+from django import forms
 
 from home.models import PublicMessage
 
 class PublicMessageForm(ModelForm):
+    body = forms.CharField(
+        widget=forms.Textarea()
+    )
     class Meta:
         model = PublicMessage
         fields = ("name", "email", "phone", "subject", "body")
