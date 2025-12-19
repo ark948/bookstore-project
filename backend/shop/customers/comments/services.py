bd4dd4d05a3e9fbaac821408defa8f2b5139218c
ark948 <armankarimi948@gmail.com>
2025-12-19 20:10:40 +0330

    Simple utility function to check if vote exists

---
+++ backend/shop/customers/comments/services.py
@@ -0,0 +1,16 @@
+from django.shortcuts import get_object_or_404
+from shop.models import Vote, Comment
+
+def check_vote_exists(comment_id: int, user):
+    comment_obj = Comment.objects.get(pk=comment_id)
+    if comment_obj:
+        item = Vote.objects.filter(
+            comment=comment_obj,
+            user=user
+        ).first()
+        if item.exists():
+            return True
+        else:
+            return False
+    return None
+
