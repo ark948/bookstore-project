f50eac9a426f8961ee1fb65377d85783462d732a
ark948 <armankarimi948@gmail.com>
2025-12-20 22:27:04 +0330

    Is book favorite feature OK (only in browse section for now)

--- backend/shop/customers/favorites/views.py
+++ backend/shop/customers/favorites/views.py
@@ -28,7 +28,7 @@ def remove_book_favorite(request: HttpRequest, book_id: int) -> HttpResponse:
 @role_required('user')
 def is_book_favorite(request: HttpRequest, book_id: int) -> HttpResponse:
     if request.htmx:
-        favorite: QuerySet = Favorite.objects.filter( user_id=request.user, book_id=book_id ).first()
+        favorite: QuerySet = Favorite.objects.filter(user_id=request.user, book_id=book_id).first()
         if favorite:
             return render(request, "shop/customers/favorites/partials/remove_from_favorite_partial.html", {'book_id': book_id})
         else:
