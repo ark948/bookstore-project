e729a24a3cfb70dfe4db83b2a9b7b447a6e5492f
ark948 <armankarimi948@gmail.com>
2025-12-20 22:04:06 +0330

    Add to favroite OK

--- backend/shop/customers/favorites/views.py
+++ backend/shop/customers/favorites/views.py
@@ -11,11 +11,10 @@ def add_book_favorite(request: HttpRequest, book_id: int) -> HttpResponse:
     if request.htmx:
         book_obj = get_object_or_404(Book, pk=book_id)
         try:
-            Favorite.objects.create( user_id=request.user, book_id = book_obj )
+            Favorite.objects.create(user_id=request.user, book_id=book_obj)
         except Exception as error:
-            print("\nERROR:", error, "\n")
             return HttpResponse("خطا")
-        return render(request, "shop/customers/partials/remove_from_favorite_partial.html", {'book_id': book_id})
+        return render(request, "shop/customers/favorites/partials/remove_from_favorite_partial.html", {'book_id': book_id})
     
 
 @role_required('user')
@@ -30,9 +29,7 @@ def remove_book_favorite(request: HttpRequest, book_id: int) -> HttpResponse:
 
 @role_required('user')
 def is_book_favorite(request: HttpRequest, book_id: int) -> HttpResponse:
-    print("CALLED")
     if request.htmx:
-        print("HTMX True")
         favorite: QuerySet = Favorite.objects.filter( user_id=request.user, book_id=book_id ).first()
         if favorite:
             return render(request, "shop/customers/favorites/partials/remove_from_favorite_partial.html", {'book_id': book_id})
