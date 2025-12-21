23894b5d7fde928e4da23cbb1713f92062285521
ark948 <armankarimi948@gmail.com>
2025-12-21 20:29:22 +0330

    Added refresh button for comments in book details page

--- backend/shop/customers/comments/views.py
+++ backend/shop/customers/comments/views.py
@@ -45,6 +45,7 @@ def load_book_comments(request: HttpRequest, book_id: int) -> HttpResponse:
     if comments.exists():
         context['items'] = comments
         context['comment_form'] = form
+        context['book_id'] = book_id
     if request.htmx:
         return render(
             request, 
