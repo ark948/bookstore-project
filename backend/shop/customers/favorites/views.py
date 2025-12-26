529997d7426ad14897378a976d0b6fc9379783ef
ark948 <armankarimi948@gmail.com>
2025-12-26 23:42:43 +0330

    Refactoring customers/favorites (the entire process)

--- backend/shop/customers/favorites/views.py
+++ backend/shop/customers/favorites/views.py
@@ -7,6 +7,16 @@ from shop.models import Book, Favorite
 
 
 @role_required('user')
+def is_book_favorite(request: HttpRequest, book_id: int) -> HttpResponse:
+    if request.htmx:
+        favorite: QuerySet = Favorite.objects.filter(user_id=request.user, book_id=book_id).first()
+        if favorite:
+            return render(request, "shop/customers/favorites/partials/remove_from_favorite_partial.html", {'book_id': book_id})
+        else:
+            return render(request, "shop/customers/favorites/partials/add_to_favorite_partial.html", {'book_id': book_id})
+
+
+@role_required('user')
 def add_book_favorite(request: HttpRequest, book_id: int) -> HttpResponse:
     if request.htmx:
         book_obj = get_object_or_404(Book, pk=book_id)
@@ -23,16 +33,6 @@ def remove_book_favorite(request: HttpRequest, book_id: int) -> HttpResponse:
         book_obj = get_object_or_404(Book, pk=book_id)
         Favorite.objects.filter(user_id=request.user, book_id=book_obj).delete()
         return render(request, "shop/customers/favorites/partials/add_to_favorite_partial.html", {'book_id': book_id})
-    
-
-@role_required('user')
-def is_book_favorite(request: HttpRequest, book_id: int) -> HttpResponse:
-    if request.htmx:
-        favorite: QuerySet = Favorite.objects.filter(user_id=request.user, book_id=book_id).first()
-        if favorite:
-            return render(request, "shop/customers/favorites/partials/remove_from_favorite_partial.html", {'book_id': book_id})
-        else:
-            return render(request, "shop/customers/favorites/partials/add_to_favorite_partial.html", {'book_id': book_id})
         
     
 @role_required('user')
