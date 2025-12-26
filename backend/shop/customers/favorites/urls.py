fc668a4bb85c789e26a45f9fc567e29057fd4a25
ark948 <armankarimi948@gmail.com>
2025-12-26 23:46:24 +0330

    Unused view removed from customers/favorites (check_favorite_exists)

--- backend/shop/customers/favorites/urls.py
+++ backend/shop/customers/favorites/urls.py
@@ -4,7 +4,6 @@ from shop.customers.favorites import views
 
 urlpatterns = [
     path("modify-favorite/<int:book_id>/<str:action>/", views.modify_favorite, name='modify_favorite'),
-    path("check-favorite/<int:book_id>/", views.check_favorite_exists, name='check_favorite'),
     path("remove-from-favorites/<int:book_id>/", views.remove_book_favorite, name='remove_favorite'),
     path("add-to-favorite/<int:book_id>/", views.add_book_favorite, name='add_favorite'),
     path("is-book-favorite/<int:book_id>/", views.is_book_favorite, name='is_book_favorite'),
--- backend/shop/customers/favorites/views.py
+++ backend/shop/customers/favorites/views.py
@@ -9,11 +9,9 @@ from shop.models import Book, Favorite
 @role_required('user')
 def is_book_favorite(request: HttpRequest, book_id: int) -> HttpResponse:
     if request.htmx:
-        favorite: QuerySet = Favorite.objects.filter(user_id=request.user, book_id=book_id).first()
-        if favorite:
+        if Favorite.objects.filter(user_id=request.user, book_id=book_id).exists():
             return render(request, "shop/customers/favorites/partials/remove_from_favorite_partial.html", {'book_id': book_id})
-        else:
-            return render(request, "shop/customers/favorites/partials/add_to_favorite_partial.html", {'book_id': book_id})
+        return render(request, "shop/customers/favorites/partials/add_to_favorite_partial.html", {'book_id': book_id})
 
 
 @role_required('user')
@@ -34,14 +32,6 @@ def remove_book_favorite(request: HttpRequest, book_id: int) -> HttpResponse:
         Favorite.objects.filter(user_id=request.user, book_id=book_obj).delete()
         return render(request, "shop/customers/favorites/partials/add_to_favorite_partial.html", {'book_id': book_id})
         
-    
-@role_required('user')
-def check_favorite_exists(request: HttpRequest, book_id: int) -> HttpResponse:
-    if request.htmx:
-        if Favorite.objects.filter(user_id=request.user, book_id=book_id).filter().exists():
-            return render(request, "shop/customers/favorites/partials/is_favorite.html", {'book_id': book_id})
-        return render(request, "shop/customers/favorites/partials/not_favorite.html", {'book_id': book_id})
-    
 
 @role_required('user')
 def modify_favorite(request: HttpRequest, book_id: int, action: str) -> HttpResponse:
