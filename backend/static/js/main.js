3a627967500ceba6d1c19e6c85046c1ea796c6b3
ark948 <armankarimi948@gmail.com>
2026-02-07 14:51:27 +0330

    Added jquery to static/js/main.js

--- backend/static/js/main.js
+++ backend/static/js/main.js
@@ -2,8 +2,24 @@ import '@/css/main.css';
 import '@/js/main.js';
 import '@/js/select2.min.js';
 import htmx from 'htmx.org';
+import $ from 'jquery';
 
 console.log("yo yo");
 
 // Make HTMX globally available
 window.htmx = htmx;
+
+// Make jquery globally available
+window.$ = $;
+window.jQuery = $; // Not sure about this one
+if (window.jQuery) {
+    console.log("jquery loaded.", jQuery.fn.jquery);
+} else {
+    console.error("jquery did not load.");
+}
+
+if (window.$) {
+    console.log("jquery ok.", jQuery.fn.jquery);
+} else {
+    console.error("jquery not ok.");
+}
